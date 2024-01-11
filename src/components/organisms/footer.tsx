b05ebce4f513b4c7967c16d9a2c2867e95189b4e
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-11 11:03:09 +0700

    footer

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -1,5 +1,70 @@
+import Link from "next/link";
 import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
 
 export default function Footer() {
-  return <footer>footer </footer>;
+  return (
+    <footer className=" py-20">
+      <div className="flex items-center justify-between py-10">
+        <div className="text-2xl font-semibold">Logo</div>
+        <ul className="flex items-center gap-3">
+          <li className="font-medium">
+            <Link href={"/"}>About</Link>
+          </li>
+          <li className="font-medium">
+            <Link href={"/"}>Contact us</Link>
+          </li>
+          <li className="font-medium">
+            <Link href={"/"}>FAQs</Link>
+          </li>
+          <li className="font-medium">
+            <Link href={"/"}>Support</Link>
+          </li>
+          <li className="font-medium">
+            <Link href={"/"}>Blog</Link>
+          </li>
+        </ul>
+        <ul className="flex items-center gap-3">
+          <li>
+            <i>
+              <FaInstagram />
+            </i>
+          </li>
+          <li>
+            <i>
+              <FaFacebook />
+            </i>
+          </li>
+          <li>
+            <i>
+              <FaTiktok />
+            </i>
+          </li>
+          <li>
+            <i>
+              <FaLinkedin />
+            </i>
+          </li>
+          <li>
+            <i>
+              <FaYoutube />
+            </i>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center justify-center gap-3 border-t border-black py-4">
+        <small>© 2023 Cerita Cetak. All rights reserved.</small>
+        <small>Privacy Policy</small>
+        <small>Terms of Service</small>
+        <small>Cookie Settings</small>
+      </div>
+    </footer>
+  );
 }
