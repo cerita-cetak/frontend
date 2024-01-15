5663a9f0d01ad0425d72940cefabbff88e87c569
KangSunat <92769271+kangsunat@users.noreply.github.com>
2024-01-15 15:45:17 +0700

    responsive

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,16 +3,16 @@ import { fakerID_ID as faker } from "@faker-js/faker";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex h-screen justify-between">
+    <main className="h-screen justify-between lg:flex">
       <section
-        className="w-4/12 shrink-0 bg-cover bg-no-repeat"
+        className="hidden w-4/12 shrink-0 bg-cover bg-no-repeat lg:block"
         style={{
           backgroundImage: `url(${faker.image.urlLoremFlickr({
             category: "flora",
           })})`,
         }}
       ></section>
-      <section className="relative mx-auto grid flex-1 grid-cols-1 content-center bg-gray-200">
+      <section className="relative mx-auto grid h-screen flex-1 grid-cols-1 content-center bg-gray-200">
         <div className="absolute right-5 top-5 flex items-center gap-3">
           <p className="text-lg font-medium text-slate-500">
             {faker.company.name()}
@@ -25,8 +25,8 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
             />
           </picture>
         </div>
-        <div className="mx-auto w-5/12">{children}</div>
-        <div className="absolute inset-x-0 bottom-5 flex flex-col items-center justify-center gap-3 py-4 md:flex-row">
+        <div className="mx-auto w-11/12 md:w-5/12">{children}</div>
+        <div className="absolute inset-x-0 bottom-0 flex flex-col items-center justify-center gap-1 py-4 md:bottom-5 md:flex-row md:gap-3">
           <small>© 2023 Cerita Cetak. All rights reserved.</small>
           <small>Privacy Policy</small>
           <small>Terms of Service</small>
